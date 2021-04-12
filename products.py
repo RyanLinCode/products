5b27e5a4c58fb0d342ea6fe8665a6621d5e35743
RyanLin <Willie003652@gmail.com>
2021-04-12 14:59:13 +0800

    add read file

--- products.py
+++ products.py
@@ -1,6 +1,18 @@
-# 二維清單 2 dimensioning
-# 大清單
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line: #遇到 '商品,字串'跳過不執行
+			continue #繼續
+		# strip() 除換行符號 .split(',')用逗點符號做切割
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+print(products)
+
+# 二維清單 2 dimensioning
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -17,12 +29,14 @@ print(products)
 # products[0][0] products[0][1] products[1][0] products[1][1]
 # 第一個0是大清單的0,第二個0是小清單的0
 
-
+# 印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 	# 小清單的 0 是名字 1 是 價格
 # encoding='utf-8' 選擇編碼
 # encoding='big5'
+
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
